eae9117d7c19d0bfb43a65dcbabf39e91d72e386
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-03 22:18:02 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Print
- * Return: 0
- *
- */
+ * main - puts line
+ * Return: void
+ * Description: use puts
+*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0; 
+	return (0);
 }
