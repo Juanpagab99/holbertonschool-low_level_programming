f278dbb9a3aba932eab88bc343a0d05ed66a09e3
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-09 16:30:13 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - plus
+ * @a: first number
+ * @b: second number
+ * Description: plus two numbers
+ * Return: sum
+*/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
