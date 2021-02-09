08f1d8bc3d9f1f376327e9e1b2ac301c03279611
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-09 21:26:00 +0000

    new change

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+int positive_or_negative(int n);
 
-#endif /* HOLBERTON_H */
+#endif /* HOLBERTON_H */B
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,7 +6,7 @@
  * Description: negative or positive
  * Return: Always 0 (Success)
  */
-void positive_or_negative(int n)
+int positive_or_negative(int n)
 {
 	if (n < 0)
 		printf("%d is negative\n", n);
