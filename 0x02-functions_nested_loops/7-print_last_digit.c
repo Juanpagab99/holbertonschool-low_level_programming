ffb84b908ad1b776d9fd4377fddafa70e5da5b28
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-09 05:55:11 +0000

    8040

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,19 @@
-#include "holbeton.h"
+#include "holberton.h"
 /**
  * print_last_digit - last digit
- * @x - number
+ * @n: number
  * Description: last digit
  * Return: void
 */
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
-	
+	int x;
+
+	x = n % 10;
+	if (n < 10)
+	{
+		x = x * -1;
+	}
+	_putchar(x + '0');
+	return (0);
 }
