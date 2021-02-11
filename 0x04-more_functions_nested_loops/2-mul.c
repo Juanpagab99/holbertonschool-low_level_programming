1880056000f29869aee61f9bb8b7364248b1c82b
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-11 18:21:54 +0000

    first

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - multiplication
+ * @a: first number
+ * @b: second number
+ * Return: result
+*/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * more_numbers - print more more numbers
+ *
+ *
+*/
+void more_numbers(void)
+{
+	
+}
