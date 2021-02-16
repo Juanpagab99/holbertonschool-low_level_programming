a2bef9154509f1726ae7a63d5d07b839c8d4ca34
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-16 20:36:28 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print array
+ * @a: variable 1
+ * @n: variable 2
+ *
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d ", a[i]);
+		}
+	}
+	printf("\n");
+}
