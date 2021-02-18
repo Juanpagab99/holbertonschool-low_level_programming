8453154b43b481fcaab3255fe8e2e248226f27f6
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-18 18:58:19 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcat - Hello world
+ * @src: first variable
+ * @dest: second variable
+ * Return: char
+ *
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	{
+		i++;
+	}
+	for (j = 0 ; src[j] != '\0' ; j++, ++i)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = 0;
+	return (dest);
+}
