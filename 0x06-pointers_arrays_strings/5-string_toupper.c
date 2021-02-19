22f747ded2a228a67ddd16c9cceb4583c23da98f
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-19 00:51:28 +0000

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper - toupper
+ * @str: string
+ * Return: char
+*/
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
