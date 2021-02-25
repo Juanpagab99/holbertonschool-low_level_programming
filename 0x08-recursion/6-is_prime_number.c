da260964886ddbf616570d76e96da0b3bc1b9ccc
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-25 03:32:36 +0000

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,5 +6,36 @@
 */
 int is_prime_number(int n)
 {
-	
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, n - 1));
+	}
+}
+/**
+ * prime - prime
+ * @x: n
+ * @i: n-1
+ * Return: int
+*/
+int prime(int x, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (x % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime(x, i - 1));
+		}
+	}
 }
