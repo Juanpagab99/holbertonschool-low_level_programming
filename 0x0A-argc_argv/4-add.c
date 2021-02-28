47fc001e78d2d3bb083c93cd88ec6e4f69f45d68
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-02-28 07:36:16 +0000

    new task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "holberton.h"
 /**
  * main - my name is
@@ -14,10 +15,41 @@ int main(int argc, char *argv[])
 
 	suma = 0;
 
-	for (i = 0 ; i < argc ; i++)
+	for (i = 1 ; i < argc ; i++)
 	{
-		suma += atoi(argv[i]);
+		if (verify(argv[i]))
+		{
+			suma += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	printf("%d\n", suma);
 	return (0);
+}
+/**
+ * verify - verify
+ * @num: number verify
+ * Return: int
+*/
+int verify(char *num)
+{
+	int j;
+
+	j = 0;
+	while (num[j] != '\0')
+	{
+		if (isdigit(num[j]))
+		{
+			j++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,1 +1,2 @@
 int mult(int n1, int n2);
+int verify(char *num);
