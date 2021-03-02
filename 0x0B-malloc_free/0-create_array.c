3e098b1ce4dc35fe4f30137240fb61042b3a5d54
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-02 20:04:20 +0000

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_array - crate an array
+ * @size: size of array
+ * @c: char to description
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+}
