7e8dfd625419fbe3028ea53c90b28b8c5bbfa4f3
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-02 21:12:06 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,4 +8,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int j;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (0);
+	}
+	for (j = 0; j < size; j++)
+	{
+		arr[j] = c;
+	}
+	return (arr);
 }
