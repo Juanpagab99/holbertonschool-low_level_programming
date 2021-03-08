c3a48624faba39fd70567abe19b2c12001baa8f2
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-08 23:58:25 +0000

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - A dog will teach you unconditional love.
+ * @d: pointer to structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		printf("%.6f\n", d->age);
+		if (d->owner == NULL)
+		{
+			printf("Name: (nil)");
+		}
+		else
+		{
+			printf("owner: %s\n", d->owner);
+		}
+	}
+}
