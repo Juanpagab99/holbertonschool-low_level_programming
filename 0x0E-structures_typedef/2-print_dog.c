9314231d638d448d7fba15f70b656aea27a1154e
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-09 00:11:30 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		printf("%.6f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 		{
 			printf("Name: %s\n", d->owner);
