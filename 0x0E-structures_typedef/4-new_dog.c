b90e8870307250615e510467131d9e47eaa89016
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-09 01:32:27 +0000

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
 	{
 	}
