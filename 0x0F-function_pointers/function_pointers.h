b9cef1d7ee7a3f79ebaef84624de97ebcec65199
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-10 21:38:14 +0000

    task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,14 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void print_elem(int elem);
+void print_elem_hex(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTION_POINTERS_H_ */
