9b022eda11947b4287c26c4e3954b32776ad5273
Juan Pablo Gaviria <juanpagab@gmail.com>
2021-03-10 21:01:52 +0000

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - BOB DYLAN
  * @name: print name
@@ -7,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL && name != NULL)
+	if (name != NULL && f != NULL)
 	{
 		f(name);
 	}
